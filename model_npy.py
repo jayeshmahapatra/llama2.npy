1f1ef5690853fa89985cbe0ddd50cca48405ff36
Jayesh <jayeshmahapatra@gmail.com>
2023-10-08 20:22:25 +0200

    ported attention and feedforward modules

--- model_npy.py
+++ model_npy.py
@@ -22,7 +22,9 @@ class ModelArgs:
     max_seq_len: int = 2048
     dropout: float = 0.0
 
+### Nunpy based classes for the model
 
+# Define a numpy based RMSNorm class
 class RMSNorm:
     def __init__(self, dim: int, eps: float):
         super().__init__()
@@ -36,6 +38,49 @@ class RMSNorm:
     def forward(self, x):
         output = self._norm(x.astype(float)).astype(x.dtype)
         return output * self.weight
+    
+    def __call__(self, x):
+        return self.forward(x)
+    
+# Define a numpy based linear class
+class NumpyLinear:
+    def __init__(self, in_features: int, out_features: int, bias=True):
+        self.weight = np.random.randn(out_features, in_features)
+        self.bias = np.random.randn(out_features) if bias else None
+
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        return self.forward(x)
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        if self.bias is not None:
+            return np.dot(x, self.weight.T) + self.bias
+        else:
+            return np.dot(x, self.weight.T)
+
+# Define a numpy based dropout class
+class NumpyDropout:
+    def __init__(self, p: float =0.5):
+        self.p = p
+
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        return self.forward_testing(x)
+    
+    def forward_testing(self, x: np.ndarray) -> np.ndarray:
+        # scale by 1/(1-p) to match expected value
+        return x * (1/(1-self.p))
+    
+# Numpy based softmax function
+def NumpySoftmax(x: np.ndarray, axis: int = -1) -> np.ndarray:
+    exp_x = np.exp(x - np.max(x, axis=axis, keepdims=True))
+    return exp_x / np.sum(exp_x, axis=axis, keepdims=True)
+
+# Numpy based sigmoid function
+def NumpySigmoid(x: np.ndarray) -> np.ndarray:
+    return 1.0 / (1.0 + np.exp(-x))
+
+# Numpy based silu function
+def NumpySilu(x: np.ndarray) -> np.ndarray:
+    return x * NumpySigmoid(x)
 
 
 def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
@@ -101,7 +146,7 @@ def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
         .reshape(bs, slen, n_kv_heads * n_rep, head_dim)
     )
 
-class Attention(nn.Module):
+class Attention():
     def __init__(self, args: ModelArgs):
         super().__init__()
         self.n_kv_heads = args.n_heads if args.n_kv_heads is None else args.n_kv_heads
@@ -111,35 +156,33 @@ class Attention(nn.Module):
         self.n_local_kv_heads = self.n_kv_heads // model_parallel_size
         self.n_rep = self.n_local_heads // self.n_local_kv_heads
         self.head_dim = args.dim // args.n_heads
-        self.wq = nn.Linear(args.dim, args.n_heads * self.head_dim, bias=False)
-        self.wk = nn.Linear(args.dim, self.n_kv_heads * self.head_dim, bias=False)
-        self.wv = nn.Linear(args.dim, self.n_kv_heads * self.head_dim, bias=False)
-        self.wo = nn.Linear(args.n_heads * self.head_dim, args.dim, bias=False)
-        self.attn_dropout = nn.Dropout(args.dropout)
-        self.resid_dropout = nn.Dropout(args.dropout)
+        self.wq = NumpyLinear(args.dim, args.n_heads * self.head_dim, bias=False)
+        self.wk = NumpyLinear(args.dim, self.n_kv_heads * self.head_dim, bias=False)
+        self.wv = NumpyLinear(args.dim, self.n_kv_heads * self.head_dim, bias=False)
+        self.wo = NumpyLinear(args.n_heads * self.head_dim, args.dim, bias=False) 
+        self.attn_dropout = NumpyDropout(args.dropout) 
+        self.resid_dropout = NumpyDropout(args.dropout) 
         self.dropout = args.dropout
 
-        # use flash attention or a manual implementation?
-        self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
-        if not self.flash:
-            print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
-            mask = torch.full((1, 1, args.max_seq_len, args.max_seq_len), float("-inf"))
-            mask = torch.triu(mask, diagonal=1)
-            self.register_buffer("mask", mask)
+        
+        # create causal attention mask
+        mask = np.full((1, 1, args.max_seq_len, args.max_seq_len), float("-inf"))
+        mask = np.triu(mask, k=1)
+        self.mask = mask
 
     def forward(
         self,
-        x: torch.Tensor,
-        freqs_cos: torch.Tensor,
-        freqs_sin: torch.Tensor,
+        x: np.ndarray,
+        freqs_cos: np.ndarray,
+        freqs_sin: np.ndarray,
     ):
         bsz, seqlen, _ = x.shape
 
         # QKV
         xq, xk, xv = self.wq(x), self.wk(x), self.wv(x)
-        xq = xq.view(bsz, seqlen, self.n_local_heads, self.head_dim)
-        xk = xk.view(bsz, seqlen, self.n_local_kv_heads, self.head_dim)
-        xv = xv.view(bsz, seqlen, self.n_local_kv_heads, self.head_dim)
+        xq = xq.reshape(bsz, seqlen, self.n_local_heads, self.head_dim)
+        xk = xk.reshape(bsz, seqlen, self.n_local_kv_heads, self.head_dim)
+        xv = xv.reshape(bsz, seqlen, self.n_local_kv_heads, self.head_dim)
 
         # RoPE relative positional embeddings
         xq, xk = apply_rotary_emb(xq, xk, freqs_cos, freqs_sin)
@@ -153,20 +196,15 @@ class Attention(nn.Module):
         xk = xk.transpose(1, 2)
         xv = xv.transpose(1, 2)
 
-        # flash implementation
-        if self.flash:
-            output = torch.nn.functional.scaled_dot_product_attention(xq, xk, xv, attn_mask=None, dropout_p=self.dropout if self.training else 0.0, is_causal=True)
-        else:
-            # manual implementation
-            scores = torch.matmul(xq, xk.transpose(2, 3)) / math.sqrt(self.head_dim)
-            assert hasattr(self, 'mask')
-            scores = scores + self.mask[:, :, :seqlen, :seqlen]   # (bs, n_local_heads, seqlen, cache_len + seqlen)
-            scores = F.softmax(scores.float(), dim=-1).type_as(xq)
-            scores = self.attn_dropout(scores)
-            output = torch.matmul(scores, xv)  # (bs, n_local_heads, seqlen, head_dim)
+        # manual implementation
+        scores = np.matmul(xq, xk.transpose(2, 3)) / np.sqrt(self.head_dim)
+        scores = scores + self.mask[:, :, :seqlen, :seqlen]   # (bs, n_local_heads, seqlen, cache_len + seqlen)
+        scores = NumpySoftmax(scores, axis=-1)
+        scores = self.attn_dropout(scores)
+        output = np.matmul(scores, xv)  # (bs, n_local_heads, seqlen, head_dim)
 
         # restore time as batch dimension and concat heads
-        output = output.transpose(1, 2).contiguous().view(bsz, seqlen, -1)
+        output = output.transpose(1, 2).reshape(bsz, seqlen, -1)
 
         # final projection into the residual stream
         output = self.wo(output)
@@ -174,21 +212,21 @@ class Attention(nn.Module):
         return output
 
 
-class FeedForward(nn.Module):
+class FeedForward():
     def __init__(self, dim: int, hidden_dim: int, multiple_of: int, dropout: float):
         super().__init__()
         hidden_dim = int(2 * hidden_dim / 3)
         hidden_dim = multiple_of * ((hidden_dim + multiple_of - 1) // multiple_of)
-        self.w1 = nn.Linear(dim, hidden_dim, bias=False)
-        self.w2 = nn.Linear(hidden_dim, dim, bias=False)
-        self.w3 = nn.Linear(dim, hidden_dim, bias=False)
-        self.dropout = nn.Dropout(dropout)
+        self.w1 = NumpyLinear(dim, hidden_dim, bias=False)
+        self.w2 = NumpyLinear(hidden_dim, dim, bias=False)
+        self.w3 = NumpyLinear(dim, hidden_dim, bias=False)
+        self.dropout = NumpyDropout(dropout)
 
     def forward(self, x):
-        return self.dropout(self.w2(F.silu(self.w1(x)) * self.w3(x)))
+        return self.dropout(self.w2(NumpySilu(self.w1(x)) * self.w3(x)))
 
 
-class TransformerBlock(nn.Module):
+class TransformerBlock():
     def __init__(self, layer_id: int, args: ModelArgs):
         super().__init__()
         self.n_heads = args.n_heads
@@ -212,7 +250,6 @@ class TransformerBlock(nn.Module):
 
 
 class Transformer(nn.Module):
-    last_loss: Optional[torch.Tensor]
 
     def __init__(self, params: ModelArgs):
         super().__init__()
@@ -220,39 +257,29 @@ class Transformer(nn.Module):
         self.vocab_size = params.vocab_size
         self.n_layers = params.n_layers
 
-        self.tok_embeddings = nn.Embedding(params.vocab_size, params.dim)
-        self.dropout = nn.Dropout(params.dropout)
-        self.layers = torch.nn.ModuleList()
+        self.tok_embeddings = np.random.randn(params.vocab_size, params.dim)
+        self.dropout = NumpyDropout(params.dropout)
+        
+        self.layers = []
         for layer_id in range(params.n_layers):
             self.layers.append(TransformerBlock(layer_id, params))
+        
         self.norm = RMSNorm(params.dim, eps=params.norm_eps)
-        self.output = nn.Linear(params.dim, params.vocab_size, bias=False)
+        self.output = NumpyLinear(params.dim, params.vocab_size, bias=False)
 
         # share the unembedding parameters with the embedding parameters
         self.tok_embeddings.weight = self.output.weight # https://paperswithcode.com/method/weight-tying
 
         # some useful precompute for the RoPE relative positional embeddings
-        freqs_cos, freqs_sin = precompute_freqs_cis(self.params.dim // self.params.n_heads, self.params.max_seq_len)
-        self.register_buffer("freqs_cos", freqs_cos, persistent=False)
-        self.register_buffer("freqs_sin", freqs_sin, persistent=False)
+        self.freqs_cos, self.freqs_sin = precompute_freqs_cis(self.params.dim // self.params.n_heads, self.params.max_seq_len)
 
-        # init all weights
-        self.apply(self._init_weights)
-        # apply special scaled init to the residual projections, per GPT-2 paper
-        for pn, p in self.named_parameters():
-            if pn.endswith('w3.weight') or pn.endswith('wo.weight'):
-                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * params.n_layers))
+        # Load all the weights
+        self.load_weights()
+        
 
-        # Initialize attribute for the loss of the last forward call. This will be set if the forward is called with a targets tensor.
-        self.last_loss = None
 
-    def _init_weights(self, module):
-        if isinstance(module, nn.Linear):
-            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
-            if module.bias is not None:
-                torch.nn.init.zeros_(module.bias)
-        elif isinstance(module, nn.Embedding):
-            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+    def load_weights():
+        pass
 
     def forward(self, tokens: torch.Tensor, targets: Optional[torch.Tensor] = None) -> torch.Tensor:
         _bsz, seqlen = tokens.shape
@@ -265,14 +292,9 @@ class Transformer(nn.Module):
             h = layer(h, freqs_cos, freqs_sin)
         h = self.norm(h)
 
-        if targets is not None:
-            # if we are given some desired targets also calculate the loss
-            logits = self.output(h)
-            self.last_loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
-        else:
-            # inference-time mini-optimization: only forward the output on the very last position
-            logits = self.output(h[:, [-1], :]) # note: using list [-1] to preserve the time dim
-            self.last_loss = None
+        # inference-time mini-optimization: only forward the output on the very last position
+        logits = self.output(h[:, [-1], :]) # note: using list [-1] to preserve the time dim
+        self.last_loss = None
 
         return logits
 
